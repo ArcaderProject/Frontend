e613ef14640152d1c5799ee3aaf5c749f2fa9a47
Mathias Wagner <germannewsmaker@gmail.com>
2026-07-03 10:03:31 +0200

    Fix timer position

--- arcaderui/timer_overlay.gd
+++ arcaderui/timer_overlay.gd
@@ -14,12 +14,14 @@ func _ready() -> void:
 func _on_timer_started(remaining_seconds: int) -> void:
 	_ensure_window()
 	_set_time(remaining_seconds, false)
+	_position_window()
 	window.show()
 	set_process(true)
 
 func _on_timer_tick(remaining_seconds: int, warn: bool) -> void:
 	_ensure_window()
 	if not window.visible:
+		_position_window()
 		window.show()
 	set_process(true)
 	_set_time(remaining_seconds, warn)
@@ -53,5 +55,9 @@ func _ensure_window() -> void:
 	label = window.get_node("Panel/Label")
 	get_tree().root.add_child(window)
 
+func _position_window() -> void:
 	var screen := DisplayServer.screen_get_size()
-	window.position = Vector2i((screen.x - window.size.x) / 2, 28)
+	var win_size := window.size
+	if win_size.x <= 0 or win_size.x > screen.x:
+		win_size = window.min_size
+	window.position = Vector2i((screen.x - win_size.x) / 2, 28)
